3508c17a741ff65505fdf9eeee6977e2ab0b6a60
Kevin Springer <KevinUSCU@outlook.com>
2017-10-30 16:22:41 -0700

    Add cors.

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 app.disable('x-powered-by')
 
+const cors = require('cors')
+var corsOptions = {
+  origin: 'http://127.0.0.1:8080/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+app.use(cors())
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 const morgan = require('morgan')
@@ -19,7 +26,6 @@ app.use((err, req, res, next) => {
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Resource not found.' })
 })
-
 
 const listener = () => console.log(`Listening on port ${port}!`)
 app.listen(port, listener)
